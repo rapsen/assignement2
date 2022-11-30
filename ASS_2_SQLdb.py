4d7ae5911f6998f84303bc0bb1174d95a6b5a981
Martin-Dus3k <dusema@seznam.cz>
2022-11-30 13:08:52 +0200

    Added mean time in failure and modified percentage

--- ASS_2_SQLdb.py
+++ ASS_2_SQLdb.py
@@ -65,42 +65,62 @@ def get_data_by_time(robotID, start, end):
     sqlSt=f"SELECT state, time FROM robot WHERE deviceId = '{robotID}' and time between " \
           f"'{start}' and '{end}' ORDER BY time ASC;"
     c.execute(sqlSt)
+
     list = c.fetchall()
-    time_RIS = 0
-    time_RPE = 0
-    time_DOWN = 0
-    for rep in range(len(list)-1):
+    stateDict = {}
+    efficiency = {}
+    up_time = 0
+    down_time = 0
+    infailure_times = []
+    for rep in range(len(list) - 1):
         start_dict = list[rep]
-        end_dict = list[rep+1]
+        end_dict = list[rep + 1]
         start_time = transfer_to_sec(start_dict["time"])
         end_time = transfer_to_sec(end_dict["time"])
+        robot_state = start_dict["state"]
+
+        # When failure is repaired
+        if start_dict["state"] == "DOWN" and end_dict["state"] != "DOWN":
+            up_time = end_time
+        # When failure appear
+        elif start_dict["state"] != "DOWN" and end_dict[
+            "state"] == "DOWN" and up_time != 0:
+            down_time = end_time
+
+        if up_time != 0 and down_time != 0:
+            infailure_time = round(down_time - up_time, 1)
+            infailure_times.append(infailure_time)
+            up_time = 0
+            down_time = 0
+
+        if robot_state not in stateDict:
+            stateDict.update({robot_state: 0})
         if start_time > end_time:
-            total_time = round(((24*3600)-start_time) + end_time, 1)
+            total_time = round(((24 * 3600) - start_time) + end_time, 1)
         else:
-            total_time = round(end_time-start_time, 1)
-        #print(f"State {start_dict['state']} Start:{start_time} End:{end_time} Total:{total_time}")
+            total_time = round(end_time - start_time, 1)
 
-        if start_dict["state"]=="READY-IDLE-STARVED":
-            time_RIS += total_time
-        elif start_dict["state"]=="READY-PROCESSING-EXECUTING":
-            time_RPE += total_time
-        elif start_dict["state"]=="DOWN":
-            time_DOWN += total_time
-    total_gathered_time = time_RIS + time_RPE + time_DOWN
-    perc_RIS = round((time_RIS * 100) / total_gathered_time,1)
-    perc_RPE = round((time_RPE * 100) / total_gathered_time,1)
-    perc_DOWN = round((time_DOWN * 100) / total_gathered_time,1)
+        initialvalue = stateDict[robot_state]
+        initialvalue += total_time
+        stateDict.update({robot_state: initialvalue})
 
-    efficiency = {"READY-IDLE-STARVED": perc_RIS , "READY-PROCESSING-EXECUTING":
-        perc_RPE , "DOWN": perc_DOWN}
-    #print(f"{robotID} RIS:{perc_RIS}% RPE:{perc_RPE}% DOWN:{perc_DOWN}%")
+    total_gathered_time = round(sum(stateDict.values()), 1)
+
+    for rep in stateDict:
+        time_state = stateDict[rep]
+        perc = round((time_state * 100) / total_gathered_time, 1)
+        efficiency.update({rep: perc})
+
+    total_fail_time = sum(infailure_times)
+    mean_time = total_fail_time / len(infailure_times)
+    efficiency.update({"MEAN": mean_time})
 
     return efficiency
 
-#robot = get_realtime_robot_state("rob1")
-#print(robot)
-
-#times = get_data_by_time("rob1", "23.11.2022 22:20", "23.11.2022 22:50")
-#print(times)
-
-#conn.close()
+# robot = get_realtime_robot_state("rob1")
+# print(robot)
+#
+# times = get_data_by_time("rob1", "26.11.2022 17:25:30", "26.11.2022 22:50")
+# print(times)
+#
+# conn.close()
