9a848c185b389595abebf5523fedc7e5de83432e
Rapsen <75133916+Rapsen@users.noreply.github.com>
2022-12-03 20:37:46 +0200

    Simplify state icons for dashboard

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -5,23 +5,7 @@ socket.on('update', function (message) {
     if (message.deviceId == id) {
         document.getElementById("time").innerHTML = message.time;
         document.getElementById("state").innerHTML = message.state;
-        let s;
-
-        switch (message.state) {
-            case 'READY-PROCESSING-EXECUTING':
-            case 'READY-PROCESSING-ACTIVE':
-                s = "processing";
-                break;
-            case 'READY-IDLE-STARVED':
-            case 'READY-IDLE-BLOCKED':
-                s = "idle";
-                break;
-            default:
-                s = "error";
-        }
-
-        document.getElementById("status").src = "../static/icons/" + s + ".png";
-
+        document.getElementById("status").src = "../static/icons/" + message.state + ".png";
     };
 });
 
