3e6c8393a57343d25893ba5d25e5998349bfa3d6
Rapsen <75133916+Rapsen@users.noreply.github.com>
2022-12-04 03:20:29 +0200

    Small edits to frontend dashboard

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,13 +1,26 @@
 const socket = io();
 
 socket.on('update', function (message) {
-    id = document.getElementById("select").value
+    id = document.getElementById("select").value;
     if (message.deviceId == id) {
-        document.getElementById("time").innerHTML = message.time;
+        document.getElementById("time").valueAsDate = time(message.time);
         document.getElementById("state").innerHTML = message.state;
         document.getElementById("status").src = "../static/icons/" + message.state + ".png";
     };
 });
+ 
+$(document).ready()
+{
+    var t = document.getElementById('time').value
+    console.log(t)
+    document.getElementById('time').value = time(t);
+}
+
+function time(ts) {
+    let time = new Date(ts*1000)
+    time.setHours(new Date(ts*1000).getHours() + 4) // Adapt to GMT+2 time
+    return time;
+}
 
 function ask(select) {
     socket.emit("ask", select.value)
